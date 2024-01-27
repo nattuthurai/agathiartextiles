772e67bdd13aff2835fb9af023860afe0235fc49
nattuthurai <nattuthurai.k@live.com>
2024-01-27 21:48:11 +0530

    Added recently

--- src/app/yarn/page.tsx
+++ src/app/yarn/page.tsx
@@ -5,11 +5,16 @@ import { useEffect, useState } from "react";
 
 const Yarn = () => {
   const [data, setData] = useState(null);
+
   const [options, setOptions] = useState([
     { key: "Select a company", value: "" },
   ]);
 
-  useEffect(() => {
+  const [optionsColor, setOptionsColor] = useState([
+    { key: "Select a color", value: "" },
+  ]);
+
+  const FetchYarn = () => {
     const results = [{ key: "", value: "" }];
     fetch("/api/yarn")
       .then((res) => res.json())
@@ -25,6 +30,29 @@ const Yarn = () => {
 
         setOptions(results);
       });
+  };
+
+  const FetchColor = () => {
+    const results = [{ key: "", value: "" }];
+    fetch("/api/color")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        data.forEach((value: any) => {
+          results.push({
+            key: value.name,
+            value: value.id,
+          });
+        });
+
+        setOptionsColor(results);
+      });
+  };
+
+  useEffect(() => {
+    FetchYarn();
+    FetchColor();
   }, []);
 
   return (
@@ -70,23 +98,31 @@ const Yarn = () => {
         </div>
         <div className="mb-5">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Your email
+            Select Color
           </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
+          <select
+            className="block appearance-none w-full bg-white border
+         border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow 
+         leading-tight focus:outline-none focus:shadow-outline"
+            name="yarnColor"
+            id="yarnColor"
+          >
+            {optionsColor.map((option) => {
+              return (
+                <option key={option.value} value={option.value}>
+                  {option.key}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <div className="mb-5">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Your password
+            Kilo gram
           </label>
           <input
-            type="password"
-            id="password"
+            type="number"
+            id="kiloGram"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
